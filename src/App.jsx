d1a71730091c22740cc978765cbba6679d0110db
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2025-08-06 10:33:12 +0545

    App2 - Before adding users feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Counter from "./features/counter/Counter";
+import PostList from "./features/posts/PostList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
   return (
     <main className="App">
-      <Counter />
+      {/* <AddPostForm /> */}
+      <PostList />
     </main>
   );
 }
