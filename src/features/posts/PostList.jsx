f4a3419ec2e963b227958f24cd6a3c11ecf9034f
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2025-08-06 11:41:33 +0545

    Import author into PostList

--- src/features/posts/PostList.jsx
+++ src/features/posts/PostList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
 
 const PostList = () => {
   const posts = useSelector(selectAllPosts);
@@ -9,6 +10,10 @@ const PostList = () => {
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+      </p>
     </article>
   ));
 
